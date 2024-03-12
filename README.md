02ff8b3ac89e4b2f34a9c2dc59332abd8b241e03
linem.davton <linemdavton@gmail.com>
2024-03-12 10:01:46 +0100

    dotfile setup script

---
+++ README.md
@@ -0,0 +1,9 @@
+# What it does not do 
+.bashrc file 
+However is does backup the .bash_aliases file 
+
+# Sets up
+- Chrome, tmux, neovim, obsidian, fish
+- Github auth for Git 
+- Config for tmux, bash_aliases, fish, neovim
+- Obsidian vault
--- setup.bash
+++ setup.bash
@@ -32,7 +32,7 @@ curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 # nvim requires FUSE
 sudo add-apt-repository universe && sudo apt install libfuse2
 
-#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo apt install ./google-chrome-stable_current_amd64.deb && rm google-chrome-stable_current_amd64.deb
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo apt install ./google-chrome-stable_current_amd64.deb && rm google-chrome-stable_current_amd64.deb
 sudo snap install --classic obsidian
 
 # cleanup
@@ -42,7 +42,6 @@ sudo apt autoremove
 sudo mkdir -p -m 755 /etc/apt/keyrings && wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg >/dev/null &&
 	sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg &&
 	echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null &&
-	sudo apt update &&
 	sudo apt install gh -y
 
 # setup git and github auth
@@ -51,11 +50,17 @@ git config --global user.email "linemdavton@gmail.com"
 
 # github auth for git
 ssh-keygen -t ed25519 -a 100 -f $HOME/.ssh/github
+#gh auth login
 gh auth setup-git
 
 # setup dotfiles and configuration
 echo ".cfg" >.gitignore
-git clone --bare https://github.com/linem-davton/.cfg.git $HOME/.cfg
+git clone --bare https://github.com/linem-davton/.cfg $HOME/.cfg
+# backup the dotfiles that already exit
+mkdir -p $HOME/.config-backup/.config/fish && \
+git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
+xargs -I{} mv {} .config-backup/{}
+
 git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout
 
 # setup obsidian
@@ -65,4 +70,4 @@ git clone https://github.com/linem-davton/obsidianvault.git $HOME
 mkdir -p $HOME/projects
 
 # auto start fish
-echo fish >$HOME/.bash
+#echo fish >$HOME/.bashrc
